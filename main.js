99ef472a3379f344c9eebdf80d38861fe83ccccc
Moheuddin Sehab <moheuddinsehab0@gmail.com>
2019-06-16 00:14:07 +0600

    more stuff

--- main.js
+++ main.js
@@ -2,8 +2,27 @@
 var form = document.getElementById("addForm");
 var itemList = document.getElementById("items");
 var filter = document.getElementById("filter");
+
 form.addEventListener("submit", addItem);
-function addItem() {
+
+itemList.addEventListener("click", removeItem);
+
+filter.addEventListener("keyup", fitlerItems);
+
+function addItem(e) {
   event.preventDefault();
+
+  var newItem = document.getElementById("item").nodeValue;
+
+  var li = document.createElement("li");
+
+  li.className = "list-group-list";
+
+  li.appendChild(document.createTextNode(newItem));
+
+  var deleteBtn = document.createElement("button");
+
+  deleteBtn.className = "btn btn-danger btn-smfloat-right delete";
+
   console.log("Hello");
 }
